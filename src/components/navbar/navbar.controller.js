19c40efdeb8107ca070d611af0d820b73559e4b7
jmanock <jonmanock@gmail.com>
2015-03-31 11:25:26 -0400

    more styles and time added

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,6 +1,27 @@
 'use strict';
 
 angular.module('fantasy')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
-  });
+.controller('NavbarCtrl', function($scope){
+  $scope.format = 'M/d/yy h:mm:ss a';
+})
+.directive('myCurrentTime', function(dateFilter){
+  return function(scope, element, attrs){
+    var format;
+
+    scope.$watch(attrs.myCurrentTime, function(value){
+      format = value;
+      updateTime();
+    });
+    function updateTime(){
+      var dt = dateFilter(new Date(), format);
+      element.text(dt);
+    }
+    function updateLater(){
+      setTimeout(function(){
+        updateTime();
+        updateLater();
+      }, 1000);
+    }
+    updateLater();
+  };
+});
