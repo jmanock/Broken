af7723f3e47b1b38c916b97dceff5b9b52fe9330
jmanock <jonmanock@gmail.com>
2015-04-01 05:26:49 -0400

    working on the standings page

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -1,5 +1,10 @@
 'use strict';
 angular.module('fantasy')
-.controller('StandingsCtrl', function(){
-
+.controller('StandingsCtrl', function(FirebaseUrl, $scope, $firebaseUrl, $stateParams){
+  function($firebaseObject){
+    $scope.profile = $firebaseObject(FirebaseUrl.child('userTeam').child($stateParams).child('team'));
+    $scope.profile.$loaded().then(function(){
+      console.log($scope.profile.name);
+    });
+  }
 });
