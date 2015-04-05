0fbc176b873bf5054800702d29411a31550f94e1
jmanock <jonmanock@gmail.com>
2015-04-05 10:10:10 -0400

    remove button works with the counter

--- src/app/profile/profile.js
+++ src/app/profile/profile.js
@@ -45,18 +45,32 @@ angular.module('fantasy')
         userTeam.update({
           name:player.Name
         });
-      }else{
-        console.log('To many fucking players!' + id);
-        id = 5;
       }
       }
     });
   };
 
-  this.removePlayer = function(id){
-    var userTeam = FirebaseUrl.child('userTeam').child(self.user.uid).child('team').child(id);
-    userTeam.remove();
+  this.removePlayer = function(id,player){
+    //var userTeam = FirebaseUrl.child('userTeam').child(self.user.uid).child('team').child(id);
 
+    this.remove(player, id);
+
+  };
+
+  this.remove = function(player, id){
+    console.log('this should be called when remove is pressed???');
+     FirebaseUrl.child('userTeam').child(self.user.uid).child('counter').transaction(function(id){
+      return(id || 0)-1;
+    }, function(err, committed, ss){
+      if(err){
+        console.log(err);
+      }else if(committed){
+        var i = ss.val();
+        var userTeam = FirebaseUrl.child('userTeam').child(self.user.uid).child('team').child(id);
+        userTeam.remove();
+        console.log(i);
+      }
+    });
   };
 
 });
