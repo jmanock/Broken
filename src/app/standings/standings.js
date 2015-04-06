8b5d358546ca18974168d07a9e19bb409513fe87
jmanock <jonmanock@gmail.com>
2015-04-06 13:12:52 -0400

    nothing working right

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -1,9 +1,23 @@
 'use strict';
 angular.module('fantasy')
-.controller('StandingsCtrl', function(FirebaseUrl, $scope, $firebaseArray){
+.controller('StandingsCtrl', function(FirebaseUrl, $scope, $firebaseObject){
 // Teams to update with own team name
-  FirebaseUrl.on('value', function(snapshot){
-    console.log(snapshot.val().teamUser[0]);
-  });
+  var self = this;
+  // $scope.profile = $firebaseObject(FirebaseUrl.child('teamUser').child(player.$id));
+  // $scope.profile.$loaded().then(function(){
+  //   console.log($scope.profile);
+  //});
+// this.searchItem();
+//   this.searchItem = function(player){
+//     self.users = $firebaseObject(FirebaseUrl.child('teamUsers').child(player.id));
+//     self.users.$loaded(function(){
+//       console.log(self.users);
+//     });
+//   };
+  $scope.profile = $firebaseObject(FirebaseUrl.child('teamUser'));
+  $scope.profile.$loaded(function(){
 
+    });
+    //console.log($scope.profile[1].name);
+  
 });
