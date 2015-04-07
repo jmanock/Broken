97171ee35603739274e2036a5ceb5767aa293def
jmanock <jonmanock@gmail.com>
2015-04-07 08:31:46 -0400

    got teams to show up on standings page

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -1,23 +1,10 @@
 'use strict';
 angular.module('fantasy')
-.controller('StandingsCtrl', function(FirebaseUrl, $scope, $firebaseObject){
+.controller('StandingsCtrl', function(FirebaseUrl, $scope, $firebaseObject, $firebaseArray){
 // Teams to update with own team name
   var self = this;
-  // $scope.profile = $firebaseObject(FirebaseUrl.child('teamUser').child(player.$id));
-  // $scope.profile.$loaded().then(function(){
-  //   console.log($scope.profile);
-  //});
-// this.searchItem();
-//   this.searchItem = function(player){
-//     self.users = $firebaseObject(FirebaseUrl.child('teamUsers').child(player.id));
-//     self.users.$loaded(function(){
-//       console.log(self.users);
-//     });
-//   };
-  $scope.profile = $firebaseObject(FirebaseUrl.child('teamUser'));
-  $scope.profile.$loaded(function(){
+  var something = FirebaseUrl + 'teamUser';
+  this.players = $firebaseObject(FirebaseUrl.child('teamUser'));
+  console.log(something);
 
-    });
-    //console.log($scope.profile[1].name);
-  
 });
