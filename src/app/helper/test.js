156bdc273cd9def7c6e8136bcc365b36123a05b6
jmanock <jonmanock@gmail.com>
2015-04-14 09:10:48 -0400

    points showing up

--- src/app/helper/test.js
+++ src/app/helper/test.js
@@ -7,4 +7,20 @@ angular.module('fantasy')
   $http.get('app/helper/playerTest.json').success(function(data){
     $scope.players = data;
   });
+  $scope.name = 'Testing';
+  var title = [];
+  var link = [];
+  title[0] = '4';
+  title[1] = '5';
+  title[2] = '4';
+  title[3] = '5';
+  link[0] = '3';
+  link[1] = '4';
+  link[2] = '4';
+  link[3] = '6';
+
+  $scope.movies = [];
+  for(var i=0; i<4; i++){
+    $scope.movies[i] = {t:title[i], l:link[i], e:title[i]-link[i]};
+  }
 });
