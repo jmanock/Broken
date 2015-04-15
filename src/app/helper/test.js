d2d525199c097b41d185bfe31f34fcb58d9a2c26
jmanock <jonmanock@gmail.com>
2015-04-15 10:27:37 -0400

    points rules set up and working

--- src/app/helper/test.js
+++ src/app/helper/test.js
@@ -1,30 +1,38 @@
 'use strict';
 angular.module('fantasy')
 .controller('TestCtrl',function($scope, $http){
-  $http.get('app/helper/test.json').success(function(data){
-    $scope.countries = data;
-  });
-  $http.get('app/helper/playerTest.json').success(function(data){
-    $scope.players = data;
-  });
+  // $http.get('app/helper/test.json').success(function(data){
+  //   $scope.countries = data;
+  // });
+  // $http.get('app/helper/playerTest.json').success(function(data){
+  //   $scope.players = data;
+  // });
   $scope.name = 'Testing';
   var par = ['4','5','4','5','3','4','3','4','5'];
-  var score = ['3','4','4','6','5','3','4','4','4'];
+  var score = ['3','3','4','6','6','3','1','4','4'];
 
   $scope.points = [];
   $scope.movies = [];
   for(var i=0; i<score.length; i++){
     $scope.movies[i] = {
+      hole:i + 1,
       par:par[i],
       score:score[i],
       points:par[i]-score[i],
       };
+      // 5 points for a hole in one
+      if($scope.movies[i].score === '1'){
+        $scope.movies[i].points = 5;
+      }
+      // -2 points for double or worse
+      if($scope.movies[i].points <= -2){
+        $scope.movies[i].points = -2;
+      }
+      // 3 points for an eagle
+      if($scope.movies[i].points >= 2 && $scope.movies[i].score !== '1'){
+        $scope.movies[i].points = 3;
+      }
 
-    // 0 points for par
-    // 1 point for birdie
-    // 3 points for eagle
-    // -1 point for bogie
-    // -2 points for double or worse
   }
 
  })
@@ -34,7 +42,7 @@ angular.module('fantasy')
      var sum = 0;
      for (var i=0; i<data.length; i++){
        sum = sum + data[i][key];
-       console.log(sum);
+
      }return sum;
    };
  });
