5f1adfb4c8e240999d366e8efe30f3a23b7e39f8
jmanock <jonmanock@gmail.com>
2015-04-18 08:39:16 -0400

    points work, saved to firebase, called every 30 seconds, some players still not showing up

--- src/app/helper/lb.js
+++ src/app/helper/lb.js
@@ -1,72 +1,47 @@
 'use strict';
+function points(){
 var request = require('request');
 var cheerio = require('cheerio');
-
-// Gets all the players links for the score card pages
-// request('https://sports.yahoo.com/golf/pga/leaderboard', function(error, response, body){
-// 	if(!error && response.statusCode === 200){
-// 		var $ = cheerio.load(body);
-// 		// Gets all the players links to the score card page
-// 		$('.player > a').each(function(i, element){
-// 			var a = $(this);
-// 			var b = a.attr('href');
-// 			// Prints out all the links with index numbers
-// 			console.log(i + ' ' + b);
-//
-// 			// Prints out all the players
-// 			console.log(i + a.text());
-// 		});
-// 	}
-// });
-
-// Test to get all pages open
+var Firebase = require('firebase');
 var url = 'https://sports.yahoo.com/golf/pga/leaderboard';
-var something = [];
+var ref = new Firebase('https://fireseedangular.firebaseio.com');
+var golfers = [];
 
+	request(url, function(error, response, body){
+		if(!error && response.statusCode === 200){
+			console.log('we got something good here');
+			var $ = cheerio.load(body);
+			var links = $('.player > a');
 
-function callBack(){
+			links.each(function(i, link){
+				var urls = $(link).attr('href');
+				var pages = ('https://sports.yahoo.com' + urls);
 
+				request(pages, function(error, response, body){
+					if(!error && response.statusCode === 200){
+						var $page = cheerio.load(body);
+						var text = $page('h1').text();
+						var eagle = $page('.eagle').text();
+						var birdie = $page('.birdie').text();
+						var bogey = $page('.bogey').text();
+						var double = $page('.dblbogey').text();
 
-}
-request(url, function(error, response, body){
-	if(!error){
-		var $ = cheerio.load(body),
-		links = $('.player > a');
-
-		links.each(function(i, link){
-			var urls = $(link).attr('href'),
-			pages = ('https://sports.yahoo.com'+urls);
-			//results++;
-
-			request(pages, function(error, response, body){
-				if(!error){
-					var $t = cheerio.load(body),
-					score = 0,
-					text = $t('h1').text(),
-					birdie = $t('.birdie').text(),
-					dblbogey = $t('.dblbogey').text(),
-					eagle = $t('.eagle').text(),
-					bogie = $t('.bogey').text();
-
-					// birdie = score + 1;
-					// bogie = score -1;
-					// dblbogey = score -2;
-					// eagle = score + 3;
-					var data = {
-						Name: text,
-						Birdies:birdie.length * 1,
-						Bogies: bogie.length * -1,
-						eagle: eagle.length * 3,
-						db: dblbogey.length * -2,
-						points: birdie.length*1 + bogie.length * -1 + eagle.length * 3 + dblbogey.length * -2 
-					};
-
-					// Score gets updated by birdie and bogies
-					console.log( data);
-
-				}
+						var data = {
+							Name: text,
+							Eagles: eagle.length * 3,
+							Birdies: birdie.length * 1,
+							Bogey: bogey.length * -1,
+							Double: double.length * -2,
+							Points: eagle.length * 3 + birdie.length * 1 + bogey.length * -1 + double.length * -2
+						};
+						golfers.push(data);
+						console.log(golfers);
+						ref.set(golfers);
+					}
+				});
 			});
-			//callBack();
-		});
-	}
-});
+		}
+	});
+}
+points();
+setInterval(points, 30000);
