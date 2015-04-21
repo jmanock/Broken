11889f743822d899c6d3a6de177031686611eef3
jmanock <jonmanock@gmail.com>
2015-04-21 11:41:18 -0400

    loading to firebase down to 5 not loading players

--- src/app/helper/cbs.js
+++ src/app/helper/cbs.js
@@ -1,8 +1,10 @@
 'use strict';
 var request = require('request');
 var cheerio = require('cheerio');
+var firebase = require('firebase');
 var url  = 'https://sports.yahoo.com/golf/pga/leaderboard';
-var count = 0;
+var ref = new firebase('https://fireseedangular.firebaseio.com');
+var golfers = [];
 
 function pages(){
   request(url, function(error, response, body){
@@ -19,19 +21,26 @@ function pages(){
   });
 }
 
-function req(page, i){
+function req(page){
   request(page, function(error, response, body){
     if(!error && response.statusCode === 200){
       var $ = cheerio.load(body);
       var name = $('h1').text();
+      var eagle = $('.eagle').text();
+      var birdie = $('.birdie').text();
+      var bogey = $('.bogey').text();
+      var double = $('dblbogey').text();
+      var points = eagle.length * 3 + birdie.length * 1 + bogey.length * -1 + double.length * -2;
       var data = {
-        Name: name
+        Name: name,
+        Points: points
       };
-      console.log(i + ' ' + data.Name);
+
       if(data.Name === ''){
         req(page);
-        count ++;
       }
+      golfers.push(data);
+      ref.set(golfers);
     }
   });
 }
