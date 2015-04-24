fbe3e970ee083fb670f3a8c030032bdc4acca3fc
jmanock <jonmanock@gmail.com>
2015-04-24 16:07:45 -0400

    adding the points together works

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -1,10 +1,29 @@
 'use strict';
 angular.module('fantasy')
-.controller('StandingsCtrl', function($scope,FirebaseUrl, $firebaseObject, $firebaseArray ){
-
+.controller('StandingsCtrl', function($scope, FirebaseUrl, $firebaseObject, $firebaseArray){
 	var self = this;
-	var ref = new Firebase('https://toga.firebaseio.com/');
-  this.players = $firebaseArray(ref);
-  $scope.toggle = true;
+
+	// Gets the leaderboard and points
+	var ref = new Firebase('https://toga.firebaseio.com');
+	this.players = $firebaseArray(ref);
+
+	// Hides the players from the team
+	$scope.toggle = false;
+
+	// Gets the teams
 	this.teams = $firebaseArray(FirebaseUrl.child('teamUser'));
-});
+})
+.filter('sumOfValue', function(){
+	return function(data, key){
+		if(typeof(data)=== 'undefined' && typeof(key)=== 'undefined'){
+			return 0;
+		}
+		var sum = 0;
+		for(var i = 0; i< data.length; i++){
+			sum = sum + data[i][key];
+		}
+		return sum;
+	}
+})
+
+
