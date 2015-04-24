9b62aeec263ee234da49fad173e43ff96c07d7b1
jmanock <jonmanock@gmail.com>
2015-04-24 06:59:44 -0400

    hide show works

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -1,10 +1,10 @@
 'use strict';
 angular.module('fantasy')
-.controller('StandingsCtrl', function(FirebaseUrl, $firebaseObject, $firebaseArray ){
+.controller('StandingsCtrl', function($scope,FirebaseUrl, $firebaseObject, $firebaseArray ){
 
 	var self = this;
 	var ref = new Firebase('https://toga.firebaseio.com/');
   this.players = $firebaseArray(ref);
-  
+  $scope.toggle = true;
 	this.teams = $firebaseArray(FirebaseUrl.child('teamUser'));
 });
