b5f7c161e1248b9e8870b09fe86f775b0df70a57
jmanock <jonmanock@gmail.com>
2015-04-27 23:44:38 -0400

    building profile back up

--- src/app/profile/profile.js
+++ src/app/profile/profile.js
@@ -1,78 +1,55 @@
 'use strict';
 angular.module('fantasy')
-.controller('SearchCtrl', function( $firebaseArray, FirebaseUrl, Auth, $stateParams, $firebaseObject){
+.controller('SearchCtrl', function(FirebaseUrl, $firebaseObject, $stateParams, Auth, $firebaseArray){
   var self = this;
-
-  // Getting the players from firebase
-  var ref = new Firebase('https://fireseedangular.firebaseio.com/');
-  this.players = $firebaseArray(ref);
-
+  // Load `Teams` and set-up current user
   this.currentUser = $firebaseArray(FirebaseUrl.child('users').child($stateParams.id));
   Auth.onAuth(function(user){
     self.user = user;
   });
 
+  // Get  `Players`
+  this.players = $firebaseObject(FirebaseUrl.child('leaderboard'));
 
-  this.currentUser.$loaded(function(){
-    self.teams = $firebaseObject(FirebaseUrl.child('userTeam').child($stateParams.id).child('team'));
-    // self.user.$loaded(function(){
-    //   self.show = (self.currentUser.uid === self.user.uid);
-    // });
-  });
+  // Get `Teams`
+  this.teams = $firebaseObject(FirebaseUrl.child('teamUsers'));
 
+  // Add `Player` to `Team`
   this.addPlayer = function(player){
-    this.incId(player);
+    // Send `Player` to `include` function
+    this.include(player);
   };
-
-  this.incId = function(player){
-    FirebaseUrl.child('userTeam').child(self.user.uid).child('counter').transaction(function(id){
-      if(id >=5){
-        console.log('no more fucking players can be added!');
+  this.include = function(player){
+    // Set up a counter to only add 5 players to a team
+    FirebaseUrl.child('userTeam').child(self.user.uid).child('counter')
+    .transaction(function(id){
+      if(id >= 5){
+        // CHANGE TO ALERT
+        console.log('Thats enough players');
       }else{
-      console.log(id);
-      return(id||0)+1;
-    }
-
+        console.log(id);
+        return(id||0)+1;
+      }
     }, function(err, committed, ss){
       if(err){
         console.log(err);
       }else if(committed){
         var id = ss.val();
-        if(id <=5){
-        var userTeam = FirebaseUrl.child('userTeam').child(self.user.uid).child('team').child(player.$id);
-        var teamUser = FirebaseUrl.child('teamUser').child(player.$id);
-        userTeam.update({
-          name:player.name
-        });
+        if(id <= 5){
+          var userTeam = FirebaseUrl.child('userTeam').child(self.user.uid)
+          .child('team');
+          var teamUser = FirebaseUrl.child('teamUser');
 
-        teamUser.update({
-          name:player.name
-        });
-      }
-      }
-    });
-  };
+          // Update both `userTeam` and `teamUser` with `Players`
+          userTeam.update({
+            name:player.Name
+          });
 
-  this.removePlayer = function(id,player){
-    this.remove(player, id);
-  };
-
-  this.remove = function(player, id){
-    console.log('this should be called when remove is pressed???');
-     FirebaseUrl.child('userTeam').child(self.user.uid).child('counter').transaction(function(id){
-      return(id || 0)-1;
-    }, function(err, committed, ss){
-      if(err){
-        console.log(err);
-      }else if(committed){
-        var i = ss.val();
-        var userTeam = FirebaseUrl.child('userTeam').child(self.user.uid).child('team').child(id);
-        var teamUser = FirebaseUrl.child('teamUser').child(id);
-        teamUser.remove();
-        userTeam.remove();
-        console.log(i);
+          teamUser.update({
+            name:player.Name
+          });
+        }
       }
     });
   };
-
 });
