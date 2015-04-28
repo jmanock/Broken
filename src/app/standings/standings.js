7b5a4bb5d08076967e51d29411a0756585a8fc84
jmanock <jonmanock@gmail.com>
2015-04-28 13:07:12 -0400

    totals working kinda

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -12,5 +12,17 @@ var self = this;
 	// Hides the `Players` from the `Team`
 	$scope.toggle = false;
 
-	
-});
+})
+.filter('sumOfValue', function(){
+	return function(data, key){
+		if(typeof(data) === 'undefinded' && typeof(key) === 'undefined'){
+			return 0;
+		} else{
+			var sum = 0;
+			for(var i = 0; i < data.length; i++){
+				sum = sum + data[i][key];
+			}
+			return sum;
+		}
+	}
+})
