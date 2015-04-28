6d3623d26f722d9d154ce624428009bfe517c866
jmanock <jonmanock@gmail.com>
2015-04-28 14:08:18 -0400

    total of poits working right

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -12,17 +12,13 @@ var self = this;
 	// Hides the `Players` from the `Team`
 	$scope.toggle = false;
 
-})
-.filter('sumOfValue', function(){
-	return function(data, key){
-		if(typeof(data) === 'undefinded' && typeof(key) === 'undefined'){
-			return 0;
-		} else{
-			var sum = 0;
-			for(var i = 0; i < data.length; i++){
-				sum = sum + data[i][key];
-			}
-			return sum;
+	// Get the `Total` of `Points` from `Team`
+	$scope.getTotal = function(){
+		var total = 0;
+		for (var i = 0; i < $scope.standings.teams.length; i++){
+			var something = $scope.standings.teams[i];
+			total += something.points;
 		}
-	}
-})
+		return total;
+	};
+});
