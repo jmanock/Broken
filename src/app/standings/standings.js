951c45d4c6bc36533fb48444989a119717c9bcd3
jmanock <jonmanock@gmail.com>
2015-05-04 05:27:28 -0400

    fixing errors

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('fantasy')
-.controller('StandingsCtrl', function(FirebaseUrl, $firebaseArray, $scope, $firebaseObject, $stateParams){
-var self = this;
+.controller('StandingsCtrl', function(FirebaseUrl, $firebaseArray, $scope, $firebaseObject){
+//var self = this;
 
 	// Get the `Team`
 	var something = $firebaseObject(FirebaseUrl.child('teamUser'));
@@ -14,14 +14,13 @@ var self = this;
 
 	$scope.ass = [];
 	something.$loaded().then(function(){
-		//console.log('loaded record:', something.$id);
 
-		angular.forEach(something, function(value, key){
-			//console.log('what is this' + key);
+		angular.forEach(something, function(value){
+			
 			angular.forEach(value, function(k){
-				//console.log(k.name + ' ' + k.points);
+				
 				$scope.ass.push(k);
-			})
+			});
 		});
 	});
 	$scope.data = something;
@@ -34,5 +33,5 @@ var self = this;
 			total += something.points;
 		}
 		return total;
-	}
+	};
 });
