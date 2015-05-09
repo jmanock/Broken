0e4a02a20dea990120bb73ba010c2103ca095f53
jmanock <jonmanock@gmail.com>
2015-05-09 09:47:35 -0400

    players and points working, now to get the user name

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -1,41 +1,27 @@
 'use strict';
 angular.module('fantasy')
-.controller('StandingsCtrl', function(FirebaseUrl, $firebaseArray, $scope, $firebaseObject, $stateParams){
-//var self = this;
+.controller('StandingsCtrl', function(FirebaseUrl, $firebaseArray, $scope, $firebaseObject){
 
-	// Get the `Players` from the `teamUser`
-	// $scope.teams = [];
-	 var teamUser = $firebaseObject(FirebaseUrl.child('teamUser'));
-	// teamUser.$loaded().then(function(){
-	// 	angular.forEach(teamUser, function(key){
-	// 		angular.forEach(key, function(play){
-	// 			 return $scope.teams.push(play);
-	// 		});
-	// 	});
-	// });
-	//$scope.data = teamUser;
+	// Set the `teamUser`
+	var teamUser = $firebaseObject(FirebaseUrl.child('teamUser'));
 
 	// Get the `Leaderboard`
 	$scope.players = $firebaseArray(FirebaseUrl.child('leaderboard'));
 
-	// Figure out how to match the leaderboard names to team names
-	$scope.test = function(){
+	// Run a function to match the `teamUser` players to `leaderboard` players
+	$scope.getTeams = function(){
 		$scope.teams = [];
-		
-		
-		angular.forEach(teamUser, function(key, value){
-			//console.log(key, value);
+		angular.forEach(teamUser, function(key){
 			angular.forEach(key, function(play){
-				
 				angular.forEach($scope.players, function(leader){
 					if(play.name === leader.Name){
 						$scope.teams.push(leader);
-						console.log('winner winner chicken dinner');
 					}
 				});
 			});
 		});
 	};
+	
 	// Hides the `Players` from the `Team`
 	$scope.toggle = false;
 
