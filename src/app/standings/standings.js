28a89755bc984bfb03212d4815065ba71628e4ee
jmanock <jonmanock@gmail.com>
2015-05-25 14:26:10 -0400

    thats a few hours ill never get back stupid stupid

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -1,56 +1,75 @@
 'use strict';
 angular.module('fantasy')
-.controller('StandingsCtrl', function(FirebaseUrl, $firebaseArray, $scope, $firebaseObject){
+.controller('StandingsCtrl', function(FirebaseUrl, $firebaseArray, $firebaseObject, $scope){
 
 	// Set the `teamUser`
 	var teamUser = $firebaseObject(FirebaseUrl.child('teamUser'));
 	$scope.data = teamUser;
 
-	// Get the `Leaderboard`
-	$scope.players = $firebaseArray(FirebaseUrl.child('leaderboard'));
+	// Get the `LeaderBoard`
+	var players = $firebaseArray(FirebaseUrl.child('leaderboard'));
 
-	// Load the `Leaderboard` and pull points that match `userTeam`
-	$scope.players.$loaded(function(data){
+	// Load `LeaderBoard` and sort based on `Points`
+	players.$loaded(function(data){
+		var first = [];
 		angular.forEach(data, function(leader){
-			angular.forEach(teamUser, function(data){
-				angular.forEach(data, function(player){
-					if(player.name === leader.Name){
-						player.points = leader.Points;
+			first.push(leader);
+			first.sort(function(a,b){
+				return b.Points - a.Points;
+			}); // END SORT FUNCTION
+		}); // END FOR EACH FUNCTION
+
+		// Ger the `Points` from `LeaderBoard` and something
+		angular.forEach(first, function(leaderboard){
+			angular.forEach(teamUser, function(teams){
+				angular.forEach(teams, function(players){
+					if(players.name === leaderboard.Name){
+						players.points = leaderboard.Points;
 					}
 				});
 			});
 		});
-	});
-	
-	// Hides the `Players` from the `Team`
-	$scope.toggle = false;
+		var second = [];
+		var count = 0;
+		var third = [];
+		angular.forEach(first, function(some){
+			second.push(some.Points);
+		});
+		for(var i = 0; i<second.length; i++){
+			if(second[i] === second[i-1]){
+				var x = i - count;
+				third.push(x);
+				count++;
+			}else{
+				third.push(i+1);
+				count = 0;
+			}
+		}
+		var map =[];
+		for(var j = 0; j<second.length && j < third.length; j++){
+			map.push({
+				points: second[j],
+				rank: third[j]
+			});
+		}
 
-	// Add the `points` together
+		for(var k = 0; k<first.length && k<map.length; k++){
+			if(first[k].Points === map[k].points){
+				first[k].Rank = map[k].rank;
+			}
+		}
+		$scope.players = first;
+	}); // END LOAD FUNCTION
+
+// Add the `points` together
 	$scope.getTotal = function(v){
 		var total = 0;
-		angular.forEach(v, function(s){
-			total += s.points;
-		})
+		angular.forEach(v, function(ps){
+			if(ps.points === undefined){
+				ps.points = 0;
+			}
+			total += ps.points;
+		});
 		return total;
 	}
-	
-	$scope.players.$loaded(function(data){
-		var s = [];
-		for(var i = 0; i< data.length; i++){
-			var points = data[i].Points;
-			var rank;
-			s.push(points);	
-			s.sort(function(a, b){return b-a});
-			// if(s[i] === s[i-1]){
-			// 	rank = i;
-			// 	console.log(rank);
-			// }else{
-			// 	rank = i+1;
-			// 	console.log(rank);
-			// }
-			console.log(s)
-		}
-		
-	})
-		
 });
