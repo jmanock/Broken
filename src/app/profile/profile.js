5e00635b197c8c766ee769cd5fb7ea145788b4c1
jmanock <jonmanock@gmail.com>
2015-05-28 16:23:30 -0400

    a little editting

--- src/app/profile/profile.js
+++ src/app/profile/profile.js
@@ -23,40 +23,41 @@ this.add = function(p){
 };
 
 this.count = function(p){
-  // Set up a `Counter` to limit `Players` added
+  // Set-up a `Counter` to limit `Players` added
   FirebaseUrl.child('userTeam').child(self.user.uid).child('count')
   .transaction(function(count){
     if(count === null){
       count = 0;
     }
     if(count >= 5){
-      console.log('That is all the players you can have');
+      // Change to an alert or something
+      console.log('That is all the players you can have!');
     }else{
-      //console.log(id);
-      return(count ||0)+1;
+      return(count || 0)+1;
     }
-  }, function(err, committed, ss){
+  },function(err, committed, ss){
     if(err){
       console.log(err);
-    }else if(committed){      
+    }else if(committed){
       var id = ss.val()-1;
       console.log(id);
       var userTeam = FirebaseUrl.child('userTeam').child(self.user.uid).child('team').child(p.$id);
-      
+
       userTeam.update({
         name: p.Name
       });
-      
     }
   });
 };
 
+// Remove a `Player` from `Team`
 this.removePlayer = function(id){
-  this.remove( id);
+  this.remove(id);
 };
 
 this.remove = function(id){
-  FirebaseUrl.child('userTeam').child(self.user.uid).child('count').transaction(function(id){
+  FirebaseUrl.child('userTeam').child(self.user.uid).child('count')
+  .transaction(function(id){
     return(id || 0)-1;
   }, function(err, committed, ss){
     if(err){
@@ -66,37 +67,20 @@ this.remove = function(id){
       var userTeam = FirebaseUrl.child('userTeam').child(self.user.uid).child('team').child(id);
 
       userTeam.remove();
-      
       console.log(i);
-      
     }
   });
 };
 
-// Save the `Team` into the `Standings` array??
- this.save = function(){
-  /* This is used to store the team to be called in the standings page
-    * the goal is to be able to get rid of the key value in standings
-    WHAT I NEED
-      * players
-      * user
-    PROBLEMS I SEE
-      * indexing the different teams
-      * not sure what to use, ie array or hash seems like the best idea
-      * how to not make it 100 levels deep
-      * is it going to work for the standings page 
-        * easy to call for points and total
-        * also easy to reorder based on rank
-      * how long is this going to take would like to be done sometime soon
-  */
+// Save the `Team` to call in the `Standings` page
+this.save = function(){
+  // need to change this var name
   var something = FirebaseUrl.child('teams').child(self.user.fullName);
-  
+
   angular.forEach(self.teams, function(s){
     something.push({
-      player:s.name,
-      
+      player: s.name
     });
   });
-  
- };
-});
+};
+});// END CONTROLLER
