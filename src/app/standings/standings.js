6b6e8cb500c5a2d0a05934ab3e81ae61e197656c
jmanock <jonmanock@gmail.com>
2015-05-30 15:46:14 -0400

    sort function leaderboard works

--- src/app/standings/standings.js
+++ src/app/standings/standings.js
@@ -9,5 +9,17 @@ angular.module('fantasy')
 	
 	// Get the `LeaderBoard`
 	var players = $firebaseArray(FirebaseUrl.child('leaderboard'));
-	$scope.players = players;
-});
+	
+	var first = [];
+	players.$loaded(function(ps){
+		angular.forEach(ps, function(player){
+			first.push(player);
+			first.sort(function(a,b){
+				return b.Points - a.Points;
+			}); // End sort function
+		});// End for each first
+		return first;
+	}); // End loaded
+	$scope.players = first;
+	console.log(first);
+}); // End controller
