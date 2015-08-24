a8662d7434ac01c7b27b5febe396551557228b4d
jmanock <jonmanock@gmail.com>
2015-08-24 17:39:53 -0400

    working, working, working

--- src/app/standings/json/jason.js
+++ src/app/standings/json/jason.js
@@ -1,8 +1,10 @@
 'use strict';
 var fs = require('fs');
-console.log('\n *Start*\n');
+
 var content = fs.readFileSync('field.json');
 var jsonContent = JSON.parse(content);
+var url = 'http://www.pgatour.com/data/r/033/2015/scorecards';
+var request = require('request');
 
 var something = [];
 var players = jsonContent.Tournament.Players;
@@ -15,4 +17,22 @@ for(var i = 0; i<players.length; i++){
     Id:id
   });
 }
-console.log(something);
+var urls = [];
+for(var x = 0; x<something.length; x++){
+  var rId = something[x].Id;
+  var link = url+'/'+rId+'.json';
+  urls.push(link);
+}
+for(var z = 0; z<urls.length; z++){
+  request(urls[z], function(error, response, header){
+    if(!error && response.statusCode === 200){
+      var jC = JSON.parse(header);
+      console.log(jC);
+    }
+  });
+}
+
+/*
+  ToDo
+  * Got the urls, and names
+*/
