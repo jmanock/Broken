933e2d1f5cca3b936755a73fe4473ed2204a714d
jmanock <jonmanock@gmail.com>
2015-08-25 00:24:01 -0400

    getting out of controll

--- src/app/standings/json/jason.js
+++ src/app/standings/json/jason.js
@@ -1,38 +1,47 @@
 'use strict';
 var fs = require('fs');
 
-var content = fs.readFileSync('field.json');
-var jsonContent = JSON.parse(content);
+var tourneyPlayers = fs.readFileSync('field.json');
+var course = fs.readFileSync('course.json');
+var jsonContent = JSON.parse(tourneyPlayers);
+var coursePar = JSON.parse(course);
 var url = 'http://www.pgatour.com/data/r/033/2015/scorecards';
 var request = require('request');
 
-var something = [];
+var Field = [];
 var players = jsonContent.Tournament.Players;
+
 for(var i = 0; i<players.length; i++){
   var id = players[i].TournamentPlayerId;
   var parts = players[i].PlayerName.split(', ');
   var pName = parts[1]+' '+parts[0];
-  something.push({
-    Name:pName,
-    Id:id
-  });
+    Field.push({
+      Name:pName,
+      Id:id
+    });
 }
 var urls = [];
-for(var x = 0; x<something.length; x++){
-  var rId = something[x].Id;
+for(var x = 0; x<Field.length; x++){
+  var rId = Field[x].Id;
   var link = url+'/'+rId+'.json';
   urls.push({
     Links:link,
-    Name:something[x].Name
+    Name:Field[x].Name
   });
 }
+/*
+  NEED
+  * Par for holes
+  * Shots for players √
+  * Ids to match
+  * Hole Numbers √
+*/
 var thing = function(id,rounds){
   for(var i = 0; i<rounds.length; i++){
     console.log(rounds[i]);
   }
 };
 var callMe = function(Link){
-  var something = [];
   request(Link, function(error, response, header){
     if(!error && response.statusCode === 200){
       var jc = JSON.parse(header);
@@ -46,5 +55,5 @@ var callMe = function(Link){
 for(var z = 0; z<urls.length; z++){
   var link = urls[z].Links;
   var Name = urls[z].Names;
-  callMe(link,Name);
+  // callMe(link,Name);
 }
