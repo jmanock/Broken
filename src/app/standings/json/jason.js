7942d5d257c25d0c52037dc962f68e510357b057
jmanock <jonmanock@gmail.com>
2015-08-25 00:47:13 -0400

    to many functions might kill it all

--- src/app/standings/json/jason.js
+++ src/app/standings/json/jason.js
@@ -1,59 +1,53 @@
 'use strict';
+// Helpers
 var fs = require('fs');
-
-var tourneyPlayers = fs.readFileSync('field.json');
-var course = fs.readFileSync('course.json');
-var jsonContent = JSON.parse(tourneyPlayers);
-var coursePar = JSON.parse(course);
-var url = 'http://www.pgatour.com/data/r/033/2015/scorecards';
 var request = require('request');
 
-var Field = [];
-var players = jsonContent.Tournament.Players;
+// Content
+var tourneyPlayers = fs.readFileSync('field.json');
+var coursePar = fs.readFileSync('course.json');
+var Players = JSON.parse(tourneyPlayers);
+var Par = JSON.parse(coursePar);
 
-for(var i = 0; i<players.length; i++){
-  var id = players[i].TournamentPlayerId;
-  var parts = players[i].PlayerName.split(', ');
-  var pName = parts[1]+' '+parts[0];
-    Field.push({
-      Name:pName,
-      Id:id
-    });
-}
-var urls = [];
-for(var x = 0; x<Field.length; x++){
-  var rId = Field[x].Id;
-  var link = url+'/'+rId+'.json';
-  urls.push({
-    Links:link,
-    Name:Field[x].Name
-  });
-}
-/*
-  NEED
-  * Par for holes
-  * Shots for players √
-  * Ids to match
-  * Hole Numbers √
-*/
-var thing = function(id,rounds){
-  for(var i = 0; i<rounds.length; i++){
-    console.log(rounds[i]);
-  }
+// Urls
+var url = 'http://www.pgatour.com/data/r/033/2015/scorecards';
+
+// Gets all the scores runs them against par
+var points = function(id, rounds){
+  console.log(id,rounds);
 };
-var callMe = function(Link){
-  request(Link, function(error, response, header){
+
+// Calls all the files
+var req = function(links){
+  request(links, function(error, response, header){
     if(!error && response.statusCode === 200){
-      var jc = JSON.parse(header);
-      var id = jc.p.id;
-      var rounds = jc.p.rnds;
-      thing(id,rounds);
+      var data = JSON.parse(header);
+      var id = data.p.id;
+      var rounds = data.p.rnds;
+      points(id,rounds);
     }
   });
-
 };
-for(var z = 0; z<urls.length; z++){
-  var link = urls[z].Links;
-  var Name = urls[z].Names;
-  // callMe(link,Name);
+
+// Makes the links url with the id
+var urls = function(Field){
+  for(var i = 0; i<Field.length; i++){
+    var id = Field[i].Id;
+    var links = url+'/'+id+'.json';
+    req(links);
+  }
+};
+// Change these to functions
+// Getting players out of the field
+var Field = [];
+var player = Players.Tournament.Players;
+for(var i = 0; i<player.length; i++){
+  var id = player[i].TournamentPlayerId;
+  var parts = player[i].PlayerName.split(', ');
+  var pName = parts[1]+' '+parts[0];
+  Field.push({
+    Name:pName,
+    Id:id
+  });
+  urls(Field);
 }
