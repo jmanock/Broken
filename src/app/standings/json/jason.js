f203b4cabc629998b82849c81ff854f695a42d03
jmanock <jonmanock@gmail.com>
2015-08-25 17:07:59 -0400

    losing a shit ton of data

--- src/app/standings/json/jason.js
+++ src/app/standings/json/jason.js
@@ -22,7 +22,6 @@ for(var b = 0; b<ParCourse.length; b++){
       Par:parNumber
     });
   }
-  // Should send this to a function
 }
 
 var Field = [];
@@ -36,18 +35,78 @@ for(var i = 0; i<player.length; i++){
     Id:id
   });
 }
+var PlayerStats = [];
+var FinalPlayerStats = [];
+var points = 0;
 var firstRound = function(oneRound){
+  /*
+    Has to be a way where I can get the id then all the holes
+  */
   for(var i = 0; i<oneRound.length; i++){
+    var id = oneRound[i].Id;
     var holes = oneRound[i].Holes;
-    var playerId = oneRound[i].Id;
-    for(var j = 0; j<holes.length; j++){
-      var holeNumber = holes[j].cNum;
+    console.log(id);
+    for(var j = 0; j<holes.length && j<courseStats.length; j++){
+      var holeNum = holes[j].cNum;
       var score = holes[j].sc;
-      console.log(holeNumber, score, playerId);
+      var Par = courseStats[j].Par;
+      var courseNum = courseStats[j].Hole;
+      var total = Par - score;
+      if(courseNum === holeNum){
+        if(total === 0){
+          console.log('Par');
+        }else if(total === 1){
+          console.log('Birdie');
+        }else if(total >= 2){
+          console.log('Eagle or better');
+        }else if(total === -1){
+          console.log('Bogie');
+        }else if(total >= -2){
+          console.log('Double or worse');
+        }
+      }
+    
     }
-  }
 
-};
+  }
+  // for(var i = 0; i<oneRound.length; i++){
+  //   var holes = oneRound[i].Holes;
+  //   var playerId = oneRound[i].Id;
+  //   for(var j = 0; j<holes.length; j++){
+  //     var holeNumber = holes[j].cNum;
+  //     var score = holes[j].sc;
+  //     PlayerStats.push({
+  //       Hole:holeNumber,
+  //       Score:score,
+  //       PlayerId:playerId
+  //     });
+  //   }
+  // }
+  //
+  // for(var a = 0; a<PlayerStats.length; a++){
+  //   var hole = PlayerStats[a].Hole;
+  //   var Score = PlayerStats[a].Score;
+  //   var id = PlayerStats[a].PlayerId;
+  //   for(var b = 0; b<courseStats.length; b++){
+  //     var cHole = courseStats[b].Hole;
+  //     var cPar = courseStats[b].Par;
+  //     if(hole === cHole){
+  //       FinalPlayerStats.push({
+  //         Id:id,
+  //         Score:Score,
+  //         Par:cPar
+  //       });
+  //
+  //     }
+  //   }
+  // }
+  // for(var c = 0; c<FinalPlayerStats.length; c++){
+  //   var pScore = FinalPlayerStats[c].Score;
+  //   var par = FinalPlayerStats[c].Par;
+  //   var total = par - pScore;
+  //   // Not going to work, I have to get the holes into there own object
+  // }
+}; // End function
 var points = function(id,rounds){
   var roundOne = [];
   var roundTwo = [];
@@ -99,5 +158,5 @@ var Request = function(links){
 for(var a = 0; a<Field.length; a++){
   var id = Field[a].Id;
   var links = url+'/'+id+'.json';
-  Request(links);
+   Request(links);
 }
