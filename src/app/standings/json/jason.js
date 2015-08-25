9558706bbc435faa73ad46e849f95ffaef08e1e4
jmanock <jonmanock@gmail.com>
2015-08-25 10:36:18 -0400

    par check hole check

--- src/app/standings/json/jason.js
+++ src/app/standings/json/jason.js
@@ -10,6 +10,16 @@ var Par = JSON.parse(coursePar);
 
 var url = 'http://www.pgatour.com/data/r/033/2015/scorecards';
 
+var ParCourse = Par.courses;
+for(var b = 0; b<ParCourse.length; b++){
+  var courseHoles = ParCourse[b].holes;
+  for(var c = 0; c<courseHoles.length; c++){
+    var holeNumber = courseHoles[c].number;
+    var parNumber = courseHoles[c].parValue;
+    console.log(holeNumber, parNumber);
+  }
+}
+
 var Field = [];
 var player = Players.Tournament.Players;
 for(var i = 0; i<player.length; i++){
@@ -21,58 +31,58 @@ for(var i = 0; i<player.length; i++){
     Id:id
   });
 }
-var points = function(id,rounds){
-  var roundOne = [];
-  var roundTwo = [];
-  var roundThree = [];
-  var roundFour = [];
-  for(var i = 0; i<rounds.length; i++){
-    var roundNum = rounds[i].n;
-    var holes = rounds[i].holes;
-    if(roundNum === '1'){
-      roundOne.push({
-        Id:id,
-        Holes:holes
-      });
-    }else if(roundNum === '2'){
-      roundTwo.push({
-        Id:id,
-        Holes:holes
-      });
-    }else if(roundNum === '3'){
-      roundThree.push({
-        Id:id,
-        Holes:holes
-      });
-    }else if(roundNum === '4'){
-      roundFour.push({
-        Id:id,
-        Holes:holes
-      });
-    }
-  }//End for loop
-};
-
-/*
-  Todo
-  * Function for each round
-    ~ Run Scores vs Par with each id
-    ~ Store in fb
-    ~ Add points
-*/
-
-var Request = function(links){
-  request(links, function(error, response, header){
-    if(!error && response.statusCode === 200){
-        var data = JSON.parse(header);
-        var id = data.p.id;
-        var rounds = data.p.rnds;
-        points(id, rounds);
-    }
-  });
-};
-for(var a = 0; a<Field.length; a++){
-  var id = Field[a].Id;
-  var links = url+'/'+id+'.json';
-  Request(links);
-}
+// var points = function(id,rounds){
+//   var roundOne = [];
+//   var roundTwo = [];
+//   var roundThree = [];
+//   var roundFour = [];
+//   for(var i = 0; i<rounds.length; i++){
+//     var roundNum = rounds[i].n;
+//     var holes = rounds[i].holes;
+//     if(roundNum === '1'){
+//       roundOne.push({
+//         Id:id,
+//         Holes:holes
+//       });
+//     }else if(roundNum === '2'){
+//       roundTwo.push({
+//         Id:id,
+//         Holes:holes
+//       });
+//     }else if(roundNum === '3'){
+//       roundThree.push({
+//         Id:id,
+//         Holes:holes
+//       });
+//     }else if(roundNum === '4'){
+//       roundFour.push({
+//         Id:id,
+//         Holes:holes
+//       });
+//     }
+//   }//End for loop
+// };
+//
+// /*
+//   Todo
+//   * Function for each round
+//     ~ Run Scores vs Par with each id
+//     ~ Store in fb
+//     ~ Add points
+// */
+//
+// var Request = function(links){
+//   request(links, function(error, response, header){
+//     if(!error && response.statusCode === 200){
+//         var data = JSON.parse(header);
+//         var id = data.p.id;
+//         var rounds = data.p.rnds;
+//         points(id, rounds);
+//     }
+//   });
+// };
+// for(var a = 0; a<Field.length; a++){
+//   var id = Field[a].Id;
+//   var links = url+'/'+id+'.json';
+//   Request(links);
+// }
