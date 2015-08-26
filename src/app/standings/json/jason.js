ee7f8851ba93b16c2ce6b44a3ea6556980406b75
jmanock <jonmanock@gmail.com>
2015-08-26 14:04:30 -0400

    this shit is hard

--- src/app/standings/json/jason.js
+++ src/app/standings/json/jason.js
@@ -26,21 +26,54 @@ for(var i = 0; i<ParCourse.length; i++){
   } // End `HolesOnCourse` for statment
 } // End `ParCourse` for statment
 
+var Rounds = [];
+var points = function(id, rounds){
+  var totalPoints = function(Holes, Points,x){
+    console.log(Points,x);
+    // Take apart holes
+    // add Points
+    // store id, points, round num
+  }; // End `TotalPoints` Function
+  for(var i = 0; i<rounds.length; i++){
+    var points = 0;
+    var roundNum = rounds[i].n;
+    var holes = rounds[i].holes;
+
+    if(roundNum === '1'){
+       totalPoints(holes,points,1);
+    }else if(roundNum === '2'){
+       totalPoints(holes,points,2);
+    }else if(roundNum === '3'){
+       totalPoints(holes,points,3);
+    }else if(roundNum === '4'){
+       totalPoints(holes,points,4);
+    }
+
+  } // End `Rounds` For Statment
+
+}; // End `Points` Function
+
+var load = function(Links){
+  request(Links, function(error, response, header){
+    if(!error && response.statusCode === 200){
+      var data = JSON.parse(header);
+      var id = data.p.id;
+      var rounds = data.p.rnds;
+      points(id, rounds);
+    } // End `Error` Statment
+  }); // End `Request` Statment
+}; // End `Load` Function
+
 var Field = [];
 var Player = Players.Tournament.Players;
 for(var x = 0; x<Player.length; x++){
   var id = Player[x].TournamentPlayerId;
   var parts = Player[x].PlayerName.split(', ');
   var pName = parts[1] +' '+parts[0];
+  var links = url+'/'+id+'.json';
   Field.push({
     Name:pName,
     Id:id
   });
-  console.log(Field.Id);
+  load(links);
 } // End `Player` for statment
-
-for(var z = 0; z<Field.length; z++){
-  var id = Field[z].Id;
-  var links = url+'/'+id+'.json';
-
-}
