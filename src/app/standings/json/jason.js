fad3172dbbccbbe996c9453e27249c9af3898b25
jmanock <jonmanock@gmail.com>
2015-08-27 01:47:52 -0400

    hmm this is going to be long

--- src/app/standings/json/jason.js
+++ src/app/standings/json/jason.js
@@ -12,77 +12,25 @@ var Par = JSON.parse(CoursePar);
 
 var url = 'http://www.pgatour.com/data/r/033/2015/scorecards';
 
-var ParCourse = Par.courses;
-var Course = [];
-for(var i = 0; i<ParCourse.length; i++){
-  var HolesOnCourse = ParCourse[i].holes;
-  for(var j = 0; j<HolesOnCourse.length; j++){
-    var HoleNumber = HolesOnCourse[j].number;
-    var ParNumber = HolesOnCourse[j].parValue;
-    Course.push({
-      Hole:HoleNumber,
-      Par:ParNumber
+
+
+function start(){
+  var Field = [];
+  var Player = Players.Tournament.Players;
+  Player.forEach(function(i){
+    var id = i.TournamentPlayerId;
+    var parts = i.PlayerName.split(', ');
+    var pName = parts[1]+' '+parts[0];
+    var links = url+'/'+id+'.json';
+    request(links, function(error, response, header){
+      if(!error && response.statusCode === 200){
+        var data = JSON.parse(header);
+        var rounds = data.p.rnds;
+        rounds.forEach(function(x){
+          console.log(x);
+        });
+      }
     });
-  } // End `HolesOnCourse` for statment
-} // End `ParCourse` for statment
-
-var Rounds = [];
-var Final = [];
-var points = function(id, rounds){
-
-  for(var i = 0; i<rounds.length; i++){
-    var points = 0;
-    var roundNum = rounds[i].n;
-    var holes = rounds[i].holes;
-  } // End `Rounds` For Statment
-  function totalPoints(){
-    for(var i =0; i<holes.length && Course.length; i++){
-      var holeNum = holes[i].cNum;
-      var courseNum = Course[i].Hole;
-      if(holeNum === courseNum){
-        var Par = Course[i].Par;
-        var Score = holes[i].sc;
-        var total = Par - Score;
-        if(total === 0){
-          points = points;
-        }else if(total === 1){
-          points = points + 2;
-        }else if(total >= 2){
-          points = points +4;
-        }else if(total === -1){
-          points = points -1;
-        }else if(total >= -2){
-          points = points -2;
-        }
-      } // End `If` Statment
-    } // End `For` Statment
-  } // End `TotalPoints` Function
-  console.log(id, points);
-  return totalPoints();
-
-}; // End `Points` Function
-
-var load = function(Links){
-  request(Links, function(error, response, header){
-    if(!error && response.statusCode === 200){
-      var data = JSON.parse(header);
-      var id = data.p.id;
-      var rounds = data.p.rnds;
-      points(id, rounds);
-    } // End `Error` Statment
-  }); // End `Request` Statment
-}; // End `Load` Function
-
-var Field = [];
-var Player = Players.Tournament.Players;
-for(var x = 0; x<Player.length; x++){
-  var id = Player[x].TournamentPlayerId;
-  var parts = Player[x].PlayerName.split(', ');
-  var pName = parts[1] +' '+parts[0];
-  var links = url+'/'+id+'.json';
-  Field.push({
-    Name:pName,
-    Id:id
   });
-  load(links);
-} // End `Player` for statment
+}
+start();
